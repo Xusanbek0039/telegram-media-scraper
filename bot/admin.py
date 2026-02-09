3d76c194ee574b86d062099c7cf2e6d5e6415a77
Husanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2026-02-09 18:39:17 +0500

    Update

--- bot/admin.py
+++ bot/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import TelegramUser, SearchHistory
+from .models import TelegramUser, SearchHistory, DownloadHistory
 
 
 @admin.register(TelegramUser)
@@ -16,3 +16,11 @@ class SearchHistoryAdmin(admin.ModelAdmin):
     search_fields = ('query', 'user__username', 'user__first_name')
     list_filter = ('searched_at',)
     readonly_fields = ('searched_at',)
+
+
+@admin.register(DownloadHistory)
+class DownloadHistoryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'video_title', 'format_label', 'video_url', 'downloaded_at')
+    search_fields = ('video_title', 'video_url', 'user__username', 'user__first_name')
+    list_filter = ('format_label', 'downloaded_at')
+    readonly_fields = ('downloaded_at',)
