66273cb0c56c80aa22d0520b7581486a47902815
Husanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2026-02-09 18:30:30 +0500

    Update

--- bot/apps.py
+++ bot/apps.py
@@ -1,3 +1,4 @@
+import os
 from django.apps import AppConfig
 
 
@@ -5,3 +6,8 @@ class BotConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bot'
     verbose_name = 'Telegram Bot'
+
+    def ready(self):
+        if os.environ.get('RUN_MAIN') == 'true':
+            from bot.telegram_bot import start_bot
+            start_bot()
